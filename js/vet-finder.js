bb52e4d4c6dba3512be938339e2d8e2d089330fa
MartinBindon <binm1_11@uni.worc.ac.uk>
2014-07-17 13:26:59 +0100

    Google map and geolocation
    changed implementatiof maps and geolocation.

---
+++ js/vet-finder.js
@@ -0,0 +1,13 @@
+$(function() {
+        
+        //Insert code here
+        
+        });
+
+var map;
+
+function initialize() {
+	
+}
+
+//google.maps.event.addDomListener(window, 'load', initialize);
